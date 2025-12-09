9106abd632d67e16ce86e3c0e676a5437da061dc
Krzysztof Piwkowski <krispiwko@student.agh.edu.pl>
2025-12-09 19:50:12 +0100

    (WIP) Algorytm SA, funkcja oceny, funkcja poprawy

--- Project/optimize_sol.py
+++ Project/optimize_sol.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from random import random
+import numpy as np
 
 def goal_function(plan, unassigned_groups, data):
-    coeffs = {"unassigned_groups": 100}
+    coeffs = {"unassigned_groups": 500}
     coeffs_students = {
             "max_time_in_one_day": 360,
             "cost_per_minute_over_limit": 0.01,
@@ -21,8 +23,7 @@ def goal_function(plan, unassigned_groups, data):
     for student in data["student_dict"].keys():
         groups_on_day = {day_of_week: [group for group in plan.keys() if plan[group][1] == day_of_week] for day_of_week
                          in range(0, 5)}
-        print(groups_on_day)
-        marked_groups["max_time_in_one_day"] = []
+        marked_groups["max_time_in_one_day"] = [[],[],[],[],[]]
         for day_of_week in range(0, 5):
             time_sum = 0
             for group in groups_on_day[day_of_week]:
@@ -30,7 +31,7 @@ def goal_function(plan, unassigned_groups, data):
             if time_sum > coeffs_students["max_time_in_one_day"]:
                 fun_sum += coeffs_students["cost_per_minute_over_limit"] * (
                             time_sum - coeffs_students["cost_per_minute_over_limit"])
-                marked_groups["max_time_in_one_day"] += groups_on_day[day_of_week]
+                marked_groups["max_time_in_one_day"][day_of_week] += [groups_on_day[day_of_week]]
             chronological_groups = sorted(groups_on_day[day_of_week], key=lambda x: plan[x][0])
             marked_groups["window"] = []
             for i in range(1, len(chronological_groups)):
@@ -48,15 +49,54 @@ def goal_function(plan, unassigned_groups, data):
             if time_sum > coeffs_lecturers["max_time_in_one_day"]:
                 fun_sum += coeffs_lecturers["cost_per_minute_over_limit"] * (
                         time_sum - coeffs_lecturers["cost_per_minute_over_limit"])
-                marked_groups += groups_on_day[day_of_week]
+                marked_groups["max_time_in_one_day"][day_of_week]  += [groups_on_day[day_of_week]]
             chronological_groups = sorted(groups_on_day[day_of_week], key=lambda x: plan[x][0])
             for i in range(1, len(chronological_groups)):
                 if plan[chronological_groups[i]][0] - plan[chronological_groups[i - 1]][0] > coeffs_lecturers[
                         "min_window_length"]:
                     fun_sum += coeffs_lecturers["window_cost"]
-                    marked_groups += [chronological_groups[i - 1], chronological_groups[i]]
-    return fun_sum
+                    marked_groups["window"] += [chronological_groups[i - 1], chronological_groups[i]]
+    return fun_sum, marked_groups
 
+def change_plan(plan, unassigned_groups, marked_groups, data):
+    new_plan = {}
+    new_unassigned_groups = []
+    overloaded_days = []
+    for day_of_week in range(5):
+        if len(marked_groups["max_time_in_one_day"][day_of_week]) != 0:
+            overloaded_days.append(day_of_week)
+    for day_of_week in overloaded_days:
+
+
+
+    return new_plan, new_unassigned_groups
 
 def optimize_sol(plan, unassigned_groups, data):
-    pass
+    T = 500
+    alpha = 0.999
+    T_eps = 0.01
+    max_iter = 10000
+    iter = 0
+    goal_sum, marked_groups = goal_function(plan, unassigned_groups, data)
+    best_plan = plan
+    best_unassigned_groups = unassigned_groups
+    best_goal_sum = goal_sum
+    while iter < max_iter and T > T_eps:
+        new_plan, new_unassigned_groups = change_plan(plan, unassigned_groups, marked_groups, data)
+        new_goal_sum, marked_groups = goal_function(new_plan, new_unassigned_groups, data)
+        delta = new_goal_sum - goal_sum
+        if new_goal_sum < best_goal_sum:
+            best_plan = new_plan
+            best_unassigned_groups = new_unassigned_groups
+            best_goal_sum = new_goal_sum
+        if random() < np.exp(-delta / T):
+            plan = new_plan
+            unassigned_groups = new_unassigned_groups
+            goal_sum = new_goal_sum
+        T *= alpha
+        iter += 1
+    return best_plan, best_unassigned_groups, best_goal_sum
+
+
+
+
